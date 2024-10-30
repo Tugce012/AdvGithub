79391cb723bc573c1a5666434cc6186ccecfa369
Tugce <tgcyldz86@gmail.com>
2024-10-30 10:32:03 +0100

    techpro4

--- src/Runner.java
+++ src/Runner.java
@@ -17,5 +17,9 @@ public class Runner {
 
         //techpro
         System.out.println("techpro 3");
+
+        //techpro
+        System.out.println("-------------");
+        System.out.println("****************");
     }
 }
