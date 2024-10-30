bbce18bbbe7535dfa957bdd6799cbd2ea1e7e9b4
Tugce <tgcyldz86@gmail.com>
2024-10-30 09:43:44 +0100

    v2

--- src/Runner.java
+++ src/Runner.java
@@ -2,5 +2,8 @@ public class Runner {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
+        //versiyon 2
+        System.out.println("versiyon 2");
     }
 }
